e8eb20b7dfab24101ba1caccda5bd8f4a489fddd
Takashi Idobe <idobetakashi@gmail.com>
2018-05-20 13:48:43 -0500

    more changes

--- Desktop/Programming/projects/APIProjects/MALAPI/main.js
+++ Desktop/Programming/projects/APIProjects/MALAPI/main.js
@@ -4,26 +4,35 @@ var choice = animeOrManga.options[animeOrManga.selectedIndex].value;
 var page = document.getElementById('pageNumber');
 var pageChoice = page.options[page.selectedIndex].value;
 var input = document.getElementById('query');
-var inputValue = input.value;
+var query = input.value;
+var button = document.getElementByTagName('button');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    button.addEventListener('click', function() {
+    //opens up the XHR request to send to the Jikan API
+    var request = new XMLHttpRequest();
+    request.open('GET', `https://api.jikan.moe/search/${choice}/${query}${pageChoice}`, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(request.responseText);
+        console.log(request.data.responseText.result);
+        } else {
+        // We reached our target server, but it returned an error
+        }
+      };
+
+      request.onerror = function() {
+      alert("couldn't connect to the server");
+      };
+
+      request.send();
+    });
+
+  });
 
 
-//opens up the XHR request to send to the Jikan API
-var request = new XMLHttpRequest();
-request.open('GET', `https://api.jikan.moe/search/${choice}/<1>query</1>/${pageChoice}`, true);
 
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
-    console.log(data.result[0]);
-  } else {
-    // We reached our target server, but it returned an error
-
-  }
-};
-
-request.onerror = function() {
-  alert("couldn't connect to the server");
-};
-
-request.send();
 
